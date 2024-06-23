446709a821806f21ff75b9e3d207b8e19631f15c
Vishesh Sachdev <visheshsachdev04@gmail.com>
2024-06-23 12:35:45 +0530

    day_02

--- day_02/programs/day_02/src/lib.rs
+++ day_02/programs/day_02/src/lib.rs
@@ -6,7 +6,20 @@ declare_id!("Cz6CpA6NjFyHrx6fxMZgwCMebJspqw9NqZuowMRtGUAg");
 pub mod day_02 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, a: u64, b: u64, mesage: String) -> Result<()> {
+        msg!("You said {:?}", mesage);
+        msg!("You sent {} and {}", a, b);
+        Ok(())
+    }
+
+    pub fn array(ctx: Context<Initialize>, arr: Vec<u64>) -> Result<()> {
+        msg!("Your array {:?}", arr);
+        Ok(())
+    }
+
+    pub fn overflow(ctx: Context<Initialize>, a: u64, b: u64) -> Result<()> {
+        let x: u64 = a.checked_sub(b).unwrap();
+        msg!("x is {}", x);
         Ok(())
     }
 }
--- day_02/tests/day_02.ts
+++ day_02/tests/day_02.ts
@@ -10,7 +10,18 @@ describe("day_02", () => {
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize(new anchor.BN(100), new anchor.BN(100), "HEllo World").rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Array test", async () => {
+    const tx = await program.methods.array([new anchor.BN(777), new anchor.BN(120)]).rpc();
+    console.log("Your transaction signature", tx);
+  })
+
+  // failed due to overflow as used checked in program
+  it("overflow checks", async () => {
+    const tx = await program.methods.overflow(new anchor.BN(0), new anchor.BN(1)).rpc();
+    console.log("Your transaction signature", tx);
+  })
 });
